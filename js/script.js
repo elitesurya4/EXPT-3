fce0064decf2f62bf068419e2142a1d359b66892
Suryanshu Patnaik <er.suryanshupatnaik.4@gmail.com>
2023-01-20 15:08:02 +0530

    main

--- js/script.js
+++ js/script.js
@@ -11,18 +11,3 @@ accordion.forEach((element) => {
 		}
 	});
 });
-
-
-//modal box
-
-$(document).on("click","#cust_btn",function(){
-  
-	$("#myModal").modal("toggle");
-	
-  })
- 
-  $(document).on("click","#cust_btn1",function(){
-   
-	$("#myModal1").modal("toggle");
-	
-  })
